3a68e1ec226c9892ce2601402bea492ba044e1db
Neil <zooming.in@hotmail.com>
2021-05-05 23:56:48 +1000

    updates

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+function saturdayFun (val1="roller-skate") {
+    return `This Saturday, I want to ${val1}!`;
+}
+
+
+function mondayWork (val2="go to the office") {
+    return `This Monday, I will ${val2}.`;
+}
+
+function wrapAdjective (val3="*", val4) {
+    return function (val4) {
+        return `You are ${val3}${val4}${val3}!`
+    }
+}
+
+const Calculator = {
+    add (a,b) {return a+b},
+    subtract (a,b) {return a-b},
+    multiply (a,b) {return a*b},
+    divide (a,b) {return a/b}
+}
+
+
+
+function actionApplyer (integer, array) {
+    if (array.length > 0) {
+        for (let i = 0; i < array.length; i++) {
+            integer = array[i](integer);
+            console.log(integer)   
+        }
+        return integer;
+    }
+    else {
+        return integer; 
+    }
+}
